98c9a834a325fa45c6c529136248711059bc120e
Raymond Bertran <raymond.bertram@gmx.de>
2020-12-23 12:39:00 +0100

    new

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict'; 
+
+const firstName ='James';
+const lastName = 'Bro';
+
+if(firstName && lastName){
+    console.log('Thank you for typing in your Full Name'); 
+}else{
+    console.log('Error');
+}
