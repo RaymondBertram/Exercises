8371034dfb1f7a7e3496f27f803e583adfd74b52
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-12-29 09:43:11 +0100

    new

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-'use strict'; 
+'use strict';
+
+if (history.length === 0) {
+    // need information how it is printed on the website
+    // Shows that you visited that site for the first time
+} else if (history.length === 1) {
+    //Shows that you visited that site for the second time
+    // and so on... Do I need an infinit loop? 
+
 
