54cc514ddf80caa0e3b264c4c72632669f139cdd
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-04 15:48:34 +0100

    new

--- script.js
+++ script.js
@@ -5,15 +5,6 @@
 Maybe an if-statement with special condtions ?
 
 
-
-
-
-
-
-
-
-
-
 The next feature should calculate the age and execute the full name after we typed in our values.
 
 Maybe a function?
@@ -29,3 +20,24 @@ const calculateAge = function (birthYear) {
 }
 document.write(`${firstName} ${lastName} is `)
 */
+
+function calculateAge() {
+
+    var birthDate = new Date(document.getElementById("birth_date").nodeValue);
+    var birthDateDay = birth_date.getDate();
+    var birthDateMonth = birth_date.getMonth();
+    var birthDateYear = birth_date.getFullYear();
+
+    var todayDate = new Date();
+    var todayDay = todayDate.getDate();
+    var todayMonth = todayDate.getMonth();
+    var todayYear = todayDate.getFullYear();
+
+    var calculateAge = 0;
+
+
+
+
+    var output_value = birth_date;
+    document.getElementById("calculated_age").innerHTML = output_value;
+} 
