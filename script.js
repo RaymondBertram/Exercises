ee5f6cb7bcad639241f770e921a976e4e87cc0ee
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-04 16:21:26 +0100

    new

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const calculateAge = function (birthYear) {
 document.write(`${firstName} ${lastName} is `)
 */
 
-function calculateAge() {
+function calculate_Age() {
 
     var birthDate = new Date(document.getElementById("birth_date").nodeValue);
     var birthDateDay = birth_date.getDate();
@@ -33,9 +33,15 @@ function calculateAge() {
     var todayMonth = todayDate.getMonth();
     var todayYear = todayDate.getFullYear();
 
-    var calculateAge = 0;
+    var calculate_Age = 0;
 
+    if (todayMonth > birthDateMonth) calculate_Age = todayYear - birthDateYear;
+    else if (todayMonth == birthDateMonth) {
+        if (todayDay >= birthDateDay) calculate_Age = todayYear - birthDateYear;
+        else calculate_Age = todayYear - birthDateYear - 1;
 
+    }
+    else calculate_Age = todayYear - birthDateYear - 1;
 
 
     var output_value = birth_date;
