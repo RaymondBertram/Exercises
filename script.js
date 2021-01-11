d091c4ce731db957b7295bcc4814ea09036c1d1b
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-11 08:47:57 +0100

    new

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
 'use strict';
 
-/* I need an feature that shows undeath the calculator the executed sentence with the current age and the full name and a LoginQuery which shows when you last visited this website for example: First visit = This is your first time on your page, second visit = Your last call on this website was on DD.MM.YYYY and this is your second time on this page! and so on.....
-
-Maybe an if-statement with special condtions ?
-
-
-The next feature should calculate the age and execute the full name after we typed in our values.
-
-Maybe a function?
-
-
-var firstName = new
-var lastName = new
-
-const calculateAge = function (birthYear) {
-    var currentYear = new Date().getFullYear();
-
-    return currentYear - birthYear
+function ageCalculator() {
+    var userinput = document.getElementById("DOB").value;
+    var userinput1 = document.getElementById("US").value;
+    var dob = new Date(userinput);
+    if (userinput == null || userinput == '') {
+        document.getElementById("message").innerHTML = "Choose a date please!";
+        return false;
+    } else {
+        //calculate month difference from current date in time
+        var month_diff = Date.now() - dob.getTime();
+        //convert the calculated difference in date format
+        var age_dt = new Date(month_diff);
+        //extract year from date    
+        var year = age_dt.getUTCFullYear();
+        //now calculate the age of the user
+        var age = Math.abs(year - 1970);
+        //display the calculated age
+        return document.getElementById("result").innerHTML =
+            `${userinput1}'s Age is:  ${age} years. `;
+    }
 }
-document.write(`${firstName} ${lastName} is `)
-*/
-
-const button = document.getElementById('button');
-​
-​
-button.addEventListener('click', function (){
-    let date, day, month, year, birthday, age, name;
-    name = document.getElementById('nametag').value;
-    year = parseInt(document.getElementById('year').value); //umwandeln in Zahl
-    month = parseInt(document.getElementById('month').value -1); //umwandeln in Zahl, -1 da Monate bei 0 anfangen zu zählen
-    day = parseInt(document.getElementById('day').value); //umwandeln in Zahl
-    date = new Date(); //aktueles Datum
-    birthday = new Date(year, month, day); //umwandeln der eingabe in Datumsformat
-    age = Math.floor((date - birthday)/31536000000); //berechnung des alters und umrechnung in jahre
- alert(`Hello ${name}, you are ${age} years old`);
-});
